7a8427ffe87b8ab88b5f3e7f11338bfb30432d3b
Joel Smith <joely5525@gmail.com>
2014-01-24 09:28:05 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beautilist::Application.config.secret_key_base = 'd12fa9b4f4a4f31659f6e6afa437947035f44d180ac8814c5828e9499a306df4fc169b0a3d46350e025840486017c7b2644bdf7a52fb6035bc32a97cdb8cca28'
