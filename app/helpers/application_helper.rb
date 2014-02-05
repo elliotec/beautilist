377ed9734c57298a413e75a9ca05962d4336d276
elliotec <elliotecweb@gmail.com>
2014-02-05 10:43:22 -0800

    brook fix list nesting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,38 @@ module ApplicationHelper
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render(association.to_s.singularize + "_fields", f: builder)
+      render(association.to_s.singularize + "_fields", f: builder, child: @list.children.build)
     end
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
 end
+
+
+{ "utf8"=>"✓", 
+  "authenticity_token"=>"Lj7kek39fcBmqnqhR3TCYrKi0UBTDMEBrFh9In/8NDE=", 
+  "list"=> {
+    "name"=>"1028", 
+    "children_attributes"=>{
+      "0"=>{
+        "name"=>"1028a", 
+        "_destroy"=>"false"
+      }, 
+      "1391624894820"=>{
+        "children_attributes"=>{
+          "0"=>{
+            "name"=>"1028b", 
+            "_destroy"=>"false"}
+          }
+        }, 
+      "1391624896766"=>{
+        "children_attributes"=>{
+          "0"=>{
+            "name"=>"1028c", 
+            "_destroy"=>"false"
+          }
+        }
+      }
+    }
+  }, 
+  "commit"=>"Create List"
+  }
