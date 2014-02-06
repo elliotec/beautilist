902211fc2be4433d886f8ce2e89cb88384c7177d
Joel Smith <joely5525@gmail.com>
2014-02-05 21:06:09 -0800

    cleaned up old files from removed model

--- db/migrate/20140123231502_create_steps.rb
+++
@@ -1,9 +0,0 @@
-class CreateSteps < ActiveRecord::Migration
-  def change
-    create_table :steps do |t|
-      t.string :text
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20140124013400_add_list_id_to_steps.rb
+++
@@ -1,5 +0,0 @@
-class AddListIdToSteps < ActiveRecord::Migration
-  def change
-    add_column :steps, :list_id, :integer
-  end
-end
