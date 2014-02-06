0ff49187c9575ea3a3e45fe507fee5f329aaee7f
Joel Smith <joely5525@gmail.com>
2014-02-05 19:39:13 -0800

    Fixed index to hide children and created show view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,7 +2,7 @@ class ListsController < ApplicationController
   before_action :set_list, only: [:show, :edit, :update, :destroy]
 
   def index
-    @lists = List.all
+    @lists = List.all.where(parent_id: nil)
   end
 
   def new
@@ -18,6 +18,7 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,44 @@
 module ListsHelper
+  def expand_list(list)
+    if list.children.empty?
+      render inline: "<%= " + list.name + " %>"
+    else
+      for i in list.children.all
+        return expand_list(i)
+      end
+    end
+  end
+
+
+  def nested_li(objects, &block)
+    objects = objects.order(:lft) if objects.is_a? Class
+
+    return '' if objects.size == 0
+
+    output = '<ul><li>'
+    path = [nil]
+
+    objects.each_with_index do |o, i|
+      if o.parent_id != path.last
+        # We are on a new level, did we descend or ascend?
+        if path.include?(o.parent_id)
+          # Remove the wrong trailing path elements
+          while path.last != o.parent_id
+            path.pop
+            output << '</li></ul>'
+          end
+          output << '</li><li>'
+        else
+          path << o.parent_id
+          output << '<ul><li>'
+        end
+      elsif i != 0
+        output << '</li><li>'
+      end
+      output << capture(o, path.size - 1, &block)
+    end
+
+    output << '</li></ul>' * path.length
+    output.html_safe
+  end
 end
