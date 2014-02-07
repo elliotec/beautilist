c6a777524222a754c11a8fe121d4a48ed5db5bff
elliotec <elliotecweb@gmail.com>
2014-02-06 19:04:17 -0800

    root to welcome page, add bg img

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @welcome = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ HowToSite::Application.routes.draw do
   devise_for :users
   resources :lists
 
-  root 'lists#index'
+  root 'welcome#index'
 end
